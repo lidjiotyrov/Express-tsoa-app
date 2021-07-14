98b45630bc209e8a40cc199cc1221204e6524a12
lidjiotyrov <lidji.ot@justiceteam-it.com>
2021-07-14 17:24:58 +0300

    Init commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { app } from "./app";
+import * as http from 'http';
+import {MongoHelper} from "./mongo.helper";
+
+const PORT = 8080
+const server = http.createServer(app)
+server.listen(PORT)
+server.on('listening',
+    async () => { console.info(`Listening on port ${PORT}`)
+      try {
+      await MongoHelper.connect('mongodb://10.0.0.129')
+      console.info('Connected to Mongo')
+      } catch (e) {
+      console.error(e)
+      }
+});
---
+++ src/request.logger.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+
+
+const requestLoggerMiddleware = (req: Request, resp: Response, next: NextFunction) => {
+  console.info(`${req.method} ${req.originalUrl}`)
+  const start = new Date().getTime();
+  resp.on('finish', () => {
+    const elapsed = new Date().getTime() - start;
+    console.info(`${req.method} ${req.originalUrl} ${resp.statusCode} ${elapsed}ms`)
+  });
+  next();
+};
+
+export { requestLoggerMiddleware }
